03e13b3edc316e41515567e6b81ccb22cc0ecd02
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-16 09:56:54 -0400

    All tests pass

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr.Chocolate', 3, 'M', 'purple', 'nice', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'orange', 'sassy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 8, 'F', 'tabby', 'cool', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'brown', 'snazzy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 1, 'M', 'brown', 'snazzy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 7, 'F', 'black', 'sweet', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 10, 'F', 'green', 'grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (null, 5, 'F', 'gray', 'sleepy', 1);
